f7c957ba4a507225c11b3c7140f1300fd69b8e06
LAgrl <lizethariasc14@gmail.com>
2021-05-18 21:50:45 -0700

    update request with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
- };
-
+var getUserRepos = function(user) {
+  // format the gitgub api url
+  var apiUrl = "http://api.github.com/users/" + user + "/repos";
+// make a request to the url
+fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+    });
+});
+  };
+  
  getUserRepos();
