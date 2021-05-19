43015bcf7012eaaf29160df8d90400a62d05e626
LAgrl <lizethariasc14@gmail.com>
2021-05-18 22:06:29 -0700

    user search form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,14 @@
 var getUserRepos = function(user) {
-  // format the gitgub api url
-  var apiUrl = "http://api.github.com/users/" + user + "/repos";
-// make a request to the url
-fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+      console.log(response);
+      response.json().then(function(data) {
         console.log(data);
+      });
     });
-});
   };
   
- getUserRepos();
+  getUserRepos("lizariasc");
